dcb984796ba567071b1fb4ba5bc5e696c0e5559b
Gerald4102 <106319833+Gerald4102@users.noreply.github.com>
2023-05-18 14:49:05 +0100

    added flask_sqlalchemy and models

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask
+from flask import (render_template, 
+                   url_for, request) # 'url_for' will connect the links to the html files
+from models import db, Pet, app
 
-
-app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Pet Adoption'
+    return render_template('index.html')
+
+
+@app.route('/add-pet', methods=["GET", "POST"])
+def add_pet():
+    print(request.form)
+    print(request.form['name'])
+    return render_template('addpet.html')
 
 
 @app.route('/pet')
 def pet():
-    return 'This is the page showing an individual pet'
-
+    return render_template('pet.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='127.0.0.1')
+    db.create_all()     # this replaces the base.metadata.create_all call in SQLAlchemy without Flask
+    app.run(debug=True, port=8001, host='127.0.0.1')
